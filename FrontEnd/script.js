4f3b04c6c8296001d159bbe30914541a7f6dc765
Dalila <dalila.fariello@gmail.com>
2024-07-28 15:53:51 +0200

    feat: Fetching 'categories' from API & added function for filters

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,7 @@
 /*Variable or storing fetched data, not for referencing DOM elements. 
 It is used to keep the project data that will be dynamically added to the DOM*/
-let imageWorks; 
+let imageWorks;
+let categories = []
 
 
 ///////////// Variables ///////////////
@@ -11,17 +12,17 @@ const gallery = document.querySelector(".gallery");
 
 //////////// Function Fecth to get "Works" from API: ////////////
 
-async function apiCall(){
+async function worksApiCall(){
     const response = await fetch ("http://localhost:5678/api/works");
     const works = await response.json();
-    console.log("Fecthed info", works);
+    console.log("Fecthed Works", works);
     imageWorks = works;
     displayWorks(); /*Calling this function here 
     to avoids the issue of having to wait for some other code to execute before the data is shown.*/
 }
 
 ////////////// Calling the function ////////////////
-apiCall();
+worksApiCall();
 
 //////////// Function to display Works //////////////
 
@@ -45,5 +46,40 @@ function displayWorks(items = imageWorks){ // adding these parameters so when di
 
         gallery.appendChild(figure);
     });
+}
+
+//////////// Function Fecth to get "Categories" from API: ////////////
+
+async function categoriesApiCall(){
+    const categoryRespose = await fetch ("http://localhost:5678/api/categories");
+     
+    categories = await categoryRespose.json();
+    console.log("Fetched Categories", categories);
+    displayFilters();
+}
+
+////////////// Calling the function ////////////////
+categoriesApiCall ();
+
+/////////////// Function to create filters //////////////////
+
+function displayFilters(){
+    const filterDiv = document.createElement("div");
+    filterDiv.classList.add("filterDiv");
+
+    const button = document.createElement ("button");
+    button.classList.add("filterButton");
+
+    button.textContent ="Tous";
+    filterDiv.appendChild(button);
+
+  categories.forEach(category => {
+    const categoryButton = document.createElement ("button");
+    categoryButton.classList.add("filterButton");
+    categoryButton.textContent = category.name;
+    filterDiv.appendChild(categoryButton);
+    });
+
+    projectSection.appendChild(filterDiv);
 }
 
