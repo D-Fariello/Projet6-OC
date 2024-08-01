5ea90f9c8dfbee09782e3a80a5ea0684e02c5bbd
Dalila <dalila.fariello@gmail.com>
2024-08-01 13:05:27 +0200

    feat: add login and logout functions with event handling

--- FrontEnd/JavaScript/login.js
+++ FrontEnd/JavaScript/login.js
@@ -4,35 +4,69 @@ const form = document.getElementById("form");
 const loginError = document.querySelector(".login-error");
 const passwordError = document.querySelector(".password-error");
 
-/////// Error message //////
-export async function eventListenerLogin() {
+/////// LogIn + Error message //////
+
+async function eventListenerLogin() {
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
-        let messages = [];
-        if (messages.length > 0) {
-            e.preventDefault();
-        } else {
-            try {
-                const response = await fetch("http://localhost:5678/api/users/login", {
-                    method: "POST",
-                    headers: { "Content-type": "application/json" },
-                    body: JSON.stringify({ email: email.value, password: password.value })
-                });
-                if (!response.ok) {
-                    throw new Error("error" + response.status);
-                }
-                const result = await response.json();
-                window.location.href = "/FrontEnd";
-
-                window.localStorage.setItem("token", result.token);
-            } catch (error) {
-                loginError.innerText = "Une erreur est survenue lors de la connexion.";
-                console.error("Login error", error);
+        // let messages = [];
+        // if (messages.length > 0) {
+        //     e.preventDefault();
+        // } else {
+        try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({ email: email.value, password: password.value })
+            });
+            if (!response.ok) {
+                throw new Error("error" + response.status);
             }
+            const result = await response.json();
+            window.location.href = "/FrontEnd";
+
+            window.localStorage.setItem("token", result.token);
+            isLoggedIn()
+        } catch (error) {
+            loginError.innerText = "Une erreur est survenue lors de la connexion.";
+            console.error("Login error", error);
         }
-    });
+    }
+    );
 }
 ///////// Calling the Event Listener Function //////////
 
 eventListenerLogin();
+
+
+///////// LoggedIn Function ////////////
+
+function isLoggedIn() {
+    const token = localStorage.getItem("token"); // retrieves the token from local storage
+    if (token) { //checks if the token exists
+        const loginLogout = document.querySelector(".login-logout");
+
+        if (loginLogout) { // it's refering to the class
+            if (token) { // it's refering to the const token
+                loginLogout.innerText = "logout";
+                loginLogout.addEventListener('click', handleLogout); //hanldeLogout function called in response of this click action
+            } else {
+                loginLogout.innerText = "login";
+                loginLogout.removeEventListener('click', handleLogout); //hanldeLogout function called in response of this click action
+            }
+        }
+    }
+}
+
+/////////// Handle logout action/////////// 
+
+function handleLogout() {
+    localStorage.removeItem("token");
+    window.location.reload(); // Reload the page to reflect changes
+}
+
+/////// Calling the functions ///////////
+
+eventListenerLogin();
+isLoggedIn();
